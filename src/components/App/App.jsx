f0836f913cc4b8060fe50d3fa1f97063cf1099c1
Jess Sang <jessicamsang@hotmail.com>
2025-07-30 19:12:36 -0400

    set up vite.config.js

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+
+import "./App.css";
+
+import Header from "../Header/Header";
+
+export default function App() {
+  return (
+    <div className="page">
+      <div className="page__content"></div>
+    </div>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="title">Jess Sang</h1>
+    </header>
+  );
+}
