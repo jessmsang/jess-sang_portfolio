f27e7f1d5d02cf8463c765a63133aa12f4150f6d
Jess Sang <jessicamsang@hotmail.com>
2025-09-30 12:56:06 -0400

    add portfolio and edit some styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import "./Main.css";
 
 import AboutMe from "../AboutMe/AboutMe";
 import Skills from "../Skills/Skills";
+import Portfolio from "../Portfolio/Portfolio";
 
 export default function Main() {
   return (
     <section className="main">
       <AboutMe />
       <Skills />
+      <Portfolio />
     </section>
   );
 }
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,46 @@
+import "./Portfolio.css";
+
+export default function Portfolio() {
+  return (
+    <section className="portfolio" id="portfolio-section">
+      <h2 className="portfolio__title">Portfolio</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item">
+          <a
+            href="https://www.hangout.devbyjess.com/"
+            className="portfolio__list-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="portfolio__project-name">Hangout</span> -
+            Weather-Based Activity Recommendation App
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            href="https://www.wtwr-js.jumpingcrab.com/"
+            className="portfolio__list-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="portfolio__project-name">
+              WTWR (What to Wear?)
+            </span>{" "}
+            - Weather-Based Clothing Recommendation App
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            href="https://jessmsang.github.io/se_project_spots/"
+            className="portfolio__list-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="portfolio__project-name">Spots</span> - Image
+            Sharing Social Media Site
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
