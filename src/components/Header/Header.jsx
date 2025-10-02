0e9123e3eba7cb0825777d406b2f9e433c980c06
Jess Sang <jessicamsang@hotmail.com>
2025-10-02 10:58:33 -0400

    add responsive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,8 @@
 import "./Header.css";
 
-import CircleAnimation from "../CircleAnimation/CircleAnimation";
-
 export default function Header() {
   return (
     <header className="header">
-      <CircleAnimation className="circle_top-left" />
       <h1 className="header__title">JESS SANG</h1>
     </header>
   );
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import "./LandingPage.css";
 
+import CircleAnimation from "../CircleAnimation/CircleAnimation";
 import Nav from "../Nav/Nav";
 import Header from "../Header/Header";
 import AboutMe from "../AboutMe/AboutMe";
@@ -7,6 +8,7 @@ import AboutMe from "../AboutMe/AboutMe";
 export default function LandingPage() {
   return (
     <section className="landing-page">
+      <CircleAnimation className="circle_top-left" />
       <Nav />
       <Header />
       <AboutMe />
